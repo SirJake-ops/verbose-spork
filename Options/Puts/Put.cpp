3d7e5a906d0a77f66320d83417b7877d686c2f43
Jacob Pagan <jacob.pagan10@gmail.com>
2025-05-10 13:08:16 -0500

    Adding the Put class

---
+++ Options/Puts/Put.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Jacob Pagan on 5/10/25.
+//
+
+#include "Put.h"
+
+double Put::cumulative_distribution_function(double standard_deviation) const {
+    return 0.5 * (1.0 + std::erf(standard_deviation / std::sqrt(2.0)));
+}
