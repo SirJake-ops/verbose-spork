049609114790248a50b3a00bcd20c4d75d3ed694
Jacob Pagan <jacob.pagan10@gmail.com>
2025-05-22 09:20:44 -0500

    added the testing framework

---
+++ test/main_test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Jacob Pagan on 5/22/2025.
+//
+
+#include "gtest/gtest.h"
+
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
